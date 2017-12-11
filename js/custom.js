ab97fcc4f6efda4a8bd85c03e7524b74cf57c50c
TeslaPlay2 <33940924+TeslaPlay2@users.noreply.github.com>
2017-12-11 22:39:12 +0300

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(function(){
+
+	$('a[data-target^="anchor"]').bind('click.smoothscroll', function(){
+		var target = $(this).attr('href'),
+			bl_top = $(target).offset().top - 70;
+		$('body, html').animate({scrollTop: bl_top}, 700);
+		return false;
+	});
+
+
+});
+
+
+
+
+
+
