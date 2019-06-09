0116af407be98fe823c3ad9ca4fedbd69c969bcb
TeslaPlay2 <33940924+TeslaPlay2@users.noreply.github.com>
2019-06-09 14:22:41 +0300

    Add files via upload

--- zet/js/script.js
+++ zet/js/script.js
@@ -3,3 +3,9 @@ $('.happy__container').slick({
 	arrows: 0,
 	dots: true
 });
+
+$('.menu-btn').on('click', function(e) {
+	e.preventDefault;
+	$(this).toggleClass('menu-btn_active');
+	$('.menu-nav').toggleClass('menu-nav_active');
+});
