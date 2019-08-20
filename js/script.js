d54955cf316d6a9445d3f008110f4facd291cc90
TeslaPlay2 <33940924+TeslaPlay2@users.noreply.github.com>
2019-08-20 21:04:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function(){
+	$('#datepicker').datepicker({
+		dateFormat: "yy-mm-dd",
+		changeMonth: true,
+		changeYear: true,
+		buttonImage: "../img/ic_date_range.png",
+		buttonImageOnly: true,
+		minDate: 0,
+		maxDate: "+1M +5D"
+	});
+});
+
+	$(function(){
+		var from = $ ('#fromDate')
+		.datepicker({
+			dateFormat: "yy-mm-dd",
+			changeMonth: true
+		})	
+		.on( 'change', function() {
+			to.datepicker('option','minDate', getDate(this));
+		}),
+		to = $('#toDate').datepicker({
+			dateFormat: "yy-mm-dd",
+			changeMonth: true
+		})
+		.on('change', function(){
+			from.dateFormat('option','minDate', getDate(this));
+		});
+
+		function getDate (element) {
+			var	date;
+			var dateFormat = "yy-mm-dd";
+			try {
+				date = $.datepicker.parseDate(dateFormat, element.value);
+			} catch(error) {
+				date = null;
+			}
+			return date;
+		}
+	});
